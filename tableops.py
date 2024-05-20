c1a4a2632e5b63322427bf9defb8eff2e1bc9d1a
deanchristakos <dean@alum.mit.edu>
2024-05-20 18:23:18 -0400

    allows for aggregation of multiple columns and also a datefirst flag for converting a column to datetime

--- tableops.py
+++ tableops.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
 import pandas as pd
 
-def df_convert_column_to_datetime(df, column_name='Date'):
-    df[column_name] = pd.to_datetime(df[column_name])
+
+def df_convert_column_to_datetime(df, column_name='Date', dayfirst=False):
+    df[column_name] = pd.to_datetime(df[column_name], dayfirst=dayfirst)
     return df
 
 
 def df_aggregate_by_month(df, value_column, method='sum', date_column='Date'):
-    result = df.groupby(df[date_column].dt.to_period('M'))[value_column].agg(method).to_frame()
+    if isinstance(value_column, str) and isinstance(method, str):
+        result = df.groupby(df[date_column].dt.to_period('M'))[value_column].agg(method).to_frame()
+    elif isinstance(value_column, list) and isinstance(method, list):
+        result = df.groupby(df[date_column].dt.to_period('M')).agg({v:m for (v,m) in zip(value_column, method)})
+    else:
+        raise TypeError("'value_column' and 'method' arguments both have to be strings or both have to be lists")
     result = result.reset_index()
     result = result.set_index(['Date'])
-    print(type(result))
-    print(result.columns)
     return result
 
 
 def df_aggregate_by_quarter(df, value_column, method='sum', date_column='Date'):
-    result = df.groupby(df[date_column].dt.to_period('Q'))[value_column].agg(method).to_frame()
+
+    if isinstance(value_column, str) and isinstance(method, str):
+        result = df.groupby(df[date_column].dt.to_period('Q'))[value_column].agg(method).to_frame()
+    elif isinstance(value_column, list) and isinstance(method, list):
+        result = df.groupby(df[date_column].dt.to_period('Q')).agg({v:m for (v,m) in zip(value_column, method)})
+    else:
+        raise TypeError("'value_column' and 'method' arguments both have to be strings or both have to be lists")
     result = result.reset_index()
     result = result.set_index(['Date'])
-    print(type(result))
-    print(result.columns)
     return result
-    return
