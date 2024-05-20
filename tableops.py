203a86b1a9c6536c5e39e5fe11e591c2cc848703
deanchristakos <dean@alum.mit.edu>
2024-05-20 19:41:32 -0400

    added aggregation by year

--- tableops.py
+++ tableops.py
@@ -30,3 +30,16 @@ def df_aggregate_by_quarter(df, value_column, method='sum', date_column='Date'):
     result = result.reset_index()
     result = result.set_index(['Date'])
     return result
+
+
+def df_aggregate_by_year(df, value_column, method='sum', date_column='Date'):
+
+    if isinstance(value_column, str) and isinstance(method, str):
+        result = df.groupby(df[date_column].dt.to_period('Y'))[value_column].agg(method).to_frame()
+    elif isinstance(value_column, list) and isinstance(method, list):
+        result = df.groupby(df[date_column].dt.to_period('Y')).agg({v:m for (v,m) in zip(value_column, method)})
+    else:
+        raise TypeError("'value_column' and 'method' arguments both have to be strings or both have to be lists")
+    result = result.reset_index()
+    result = result.set_index(['Date'])
+    return result
